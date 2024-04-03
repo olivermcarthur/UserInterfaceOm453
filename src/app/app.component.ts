e30b139e62d1bc73e0b021833bfcf3a1ec051014
olivermcarthur <om453@bath.ac.uk>
2024-04-03 15:59:04 +0000

    got rid of the nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-navbar></app-navbar>
     <div class="content-wrapper">
       <router-outlet></router-outlet>
     </div>
