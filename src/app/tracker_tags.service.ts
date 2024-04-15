b404232211df618e4df16b80012dc6e199cd2e67
olivermcarthur <om453@bath.ac.uk>
2024-04-15 13:48:25 +0000

    demo1 changes

--- src/app/tracker_tags.service.ts
+++ src/app/tracker_tags.service.ts
@@ -92,7 +92,7 @@ private async getCollection() {
   const app = await this.realmAppService.getAppInstance(); // Get the app instance from RealmAppService
   // Get the client for 'mongodb-atlas' service and the 'auctions' database, then the 'cars' collection
   const mongo = app.currentUser?.mongoClient('mongodb-atlas');
-  const collection = mongo?.db('Demo2').collection<TrackingTag>('T_tags');
+  const collection = mongo?.db('Demo1').collection<TrackingTag>('T_tags');
   console.log("Collection object is :", collection)
 
   if (!collection) {
